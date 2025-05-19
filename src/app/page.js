b550444dfe4aee0d257b7954bc7fb5e3ff56e634
Jesús Adán Salazar Campos <adan10104334@gmail.com>
2025-05-19 07:37:43 -0600

    fix: state maangement final, added onMouseDown for handling button without being unrendered

--- src/app/page.js
+++ src/app/page.js
@@ -7,43 +7,45 @@ export default function Home() {
   const [isSelected, setIsSelected] = useState(false);
   const [isHighLighted, setIsHighLighted] = useState(false);
 
+  const handleClearInput = () => {
+    setValueInput("");
+    setIsSelected(false);
+    setIsHighLighted(false);
+  };
+
   return (
     <div
       className={`bg-[#141534] flex w-[360px] h-[60px] rounded-xl items-center font-medium ${
         isHighLighted ? "border-2 border-[#7D77FF]" : "border-none"
       }`}
     >
-      <div className="px-[20px]">
+      <div className="px-[20px] flex-1">
         {isSelected && (
           <div className="text-[0.6rem] text-[#7D77FF] font-extralight">
             Correo electrónico
           </div>
         )}
         <input
+          type="email"
           value={valueInput}
-          placeholder="Escribe tu correo electrónico"
+          placeholder={!isSelected ? "Escribe tu correo electrónico" : ""}
           onChange={(e) => setValueInput(e.target.value)}
-          className={`w-[280px] border-0 outline-0`}
+          className="w-full bg-transparent border-0 outline-0 placeholder:opacity-50"
           onFocus={() => {
             setIsSelected(true);
             setIsHighLighted(true);
           }}
-          onBlur={() =>
-            valueInput === ""
-              ? () => {
-                  setIsSelected(false);
-                  setIsHighLighted(false);
-                }
-              : setIsHighLighted(false)
-          }
+          onBlur={() => {
+            if (valueInput === "") setIsSelected(false);
+            setIsHighLighted(false);
+          }}
         />
       </div>
       {isHighLighted && (
         <button
-          onClick={() => {
-            setValueInput("");
-            setIsSelected(false);
-          }}
+          className="mr-4"
+          onMouseDown={(e) => e.preventDefault()}
+          onClick={handleClearInput}
         >
           <Image src="cancel.svg" width="25" height="25" alt="cancel icon" />
         </button>
